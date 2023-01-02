dc58679124fbf9bb6a21d110ce300a543c391b7a
TalebN <114478471+TalebN@users.noreply.github.com>
2023-01-02 19:58:10 +0200

    Add files via upload

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"fr.chalodss.spaceinvaders","l":"fr.chalodss.spaceinvaders"},{"m":"fr.chalodss.spaceinvaders","l":"fr.chalodss.spaceinvaders.classes"},{"m":"fr.chalodss.spaceinvaders","l":"fr.chalodss.spaceinvaders.utils"}];updateSearchResults();
